920edfd32757c7be9a7c15dc7b2c04b5af4be2d5
xiaozhong.tuo <xiaozhong.tuo@gmail.com>
2023-03-03 17:04:52 +0800

    add config

---
+++ config/load.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+func LoadYamlConfig(filePath string, obj interface{}) error {
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+	if err := yaml.Unmarshal([]byte(file), &obj); err != nil {
+		return err
+	}
+	return nil
+}
